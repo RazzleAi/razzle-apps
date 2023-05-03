ad8003e84701da3e1502217b79fa15b42616fd54
tomi <tomialagbe@assetreality.com>
2023-05-03 22:20:02 +0300

    add account manager

--- packages/razzle-serp-api/src/app/serp-api.ts
+++ packages/razzle-serp-api/src/app/serp-api.ts
@@ -1,5 +1,6 @@
 import { Action } from '@razzledotai/sdk'
 import { SerpApiService } from './serp-api.service'
+import { getJson } from 'serpapi'
 
 export class SerpAPI {
   constructor(private readonly serpApiService: SerpApiService) {}
@@ -9,6 +10,11 @@ export class SerpAPI {
     description: 'Search google for a query',
   })
   async search(query) {
-    return this.serpApiService.getSearchResults(query)
+    const response = await getJson("google", {
+        api_key: process.env.SERP_API_KEY, // Get your API_KEY from https://serpapi.com/manage-api-key
+        q: query,
+        location: "Austin, Texas",
+      });
+      console.log(response);
   }
 }
